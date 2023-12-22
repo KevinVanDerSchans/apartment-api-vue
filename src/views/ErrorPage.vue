7d74113e1db83bb855b7d650a0e3c7056a1f3bb5
Kevin Schans <kevinvdsd@hotmail.com>
2023-12-22 21:24:04 +0100

    Add Error Page and component

--- src/views/ErrorPage.vue
+++ src/views/ErrorPage.vue
@@ -1,11 +1,14 @@
 <script setup>
 import NavbarComponent from '../components/NavbarComponent.vue'
+import ErrorComponent from '../components/ErrorComponent.vue'
 import FooterComponent from '../components/FooterComponent.vue'
 </script>
 
 <template>
   <main>
     <NavbarComponent />
+
+    <ErrorComponent />
 
     <div class="w-full fixed bottom-0">
       <FooterComponent />
